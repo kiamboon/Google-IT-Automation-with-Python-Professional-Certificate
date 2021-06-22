84a0d69e4c8c9a2f3cfe1a35100af3e71d36d123
Kiam Boon <73866813+kiamboon@users.noreply.github.com>
2021-06-22 20:50:24 +0800

    Add files via upload

---
+++ 3. Introduction to Git and GitHub/3.1.4b all_checks.py
@@ -0,0 +1,12 @@
+#!/use/bin/env python3
+
+import os
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    pass
+
+main()
